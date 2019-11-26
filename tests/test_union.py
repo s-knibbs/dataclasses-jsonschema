cee72612b001dc65ae1eb62b314706cca652f2b8
narakhan <narakhan@disroot.org>
2019-11-26 14:30:16 +1100

    fixed test issue caused by pytest

--- tests/test_union.py
+++ tests/test_union.py
@@ -1,25 +1,25 @@
 from dataclasses import dataclass
 from typing import Union
 
+import pytest
+
 from dataclasses_jsonschema import JsonSchemaMixin
 
 
-@dataclass
-class A(JsonSchemaMixin):
-    a: str
-
-
-@dataclass
-class B(JsonSchemaMixin):
-    b: str
-
-
-@dataclass
-class C(JsonSchemaMixin):
-    c: Union[A, B]
-
-
+@pytest.mark.last
 def test_nested_union():
+    @dataclass
+    class A(JsonSchemaMixin):
+        a: str
+
+    @dataclass
+    class B(JsonSchemaMixin):
+        b: str
+
+    @dataclass
+    class C(JsonSchemaMixin):
+        c: Union[A, B]
+
     a_variant = C(A("a"))
     b_variant = C(B("a"))
 
