e322c2d5d9fdd3e573f9ad5cbe474eba4f9bf70d
narakhan <narakhan@disroot.org>
2019-11-26 13:40:17 +1100

    removed print statements

--- tests/test_union.py
+++ tests/test_union.py
@@ -23,8 +23,5 @@ def test_nested_union():
     a_variant = C(A("a"))
     b_variant = C(B("a"))
 
-    print(C.from_json(a_variant.to_json()))
-    print(C.from_json(b_variant.to_json()))
-
     assert a_variant == C.from_json(a_variant.to_json())
     assert b_variant == C.from_json(b_variant.to_json())
